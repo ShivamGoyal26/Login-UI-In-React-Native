27f4e003f03f63e552c9e3ed70460501907e69cd
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-16 12:32:25 +0530

    Splash Screen Done

--- SplashScrren/SplashScreen.js
+++ SplashScrren/SplashScreen.js
@@ -1,42 +1,59 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
 
 import LinearGradient from 'react-native-linear-gradient';
 
 const SplashScreen = props => {
     return (
 
-        <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.linearGradient}>
-            <Text style={styles.buttonText}>
-                Sign in with Facebook
-  </Text>
+        <LinearGradient colors={['#C2185B', '#ffffff']} style={styles.linearGradient}>
+            <View style={styles.splashScreen}>
+                <Image
+                    source={{ uri: 'https://i.pinimg.com/originals/12/b0/7d/12b07d7d7dbde76a3a687552d50d397f.png'}}
+                    style={styles.image}
+                />
+                <Text style={styles.titleText}>
+                    New Walmart Store
+                </Text>
+                <Text style={styles.subTitleText}>
+                    Buy whatever you want to buy, Cool Things are Waiting!
+                </Text>
+            </View>
         </LinearGradient>
-        //     <View style={styles.splashScreen}>
-        //     <Text> Splash Screen Yo An</Text>
-        // </View>
     );
 
 };
 
 const styles = StyleSheet.create({
-    // splashScreen: {
-    //     flex: 1,
-    //     backgroundColor: 'black'
-    // }
+    splashScreen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
     linearGradient: {
         flex: 1,
         paddingLeft: 15,
         paddingRight: 15,
         borderRadius: 5
-      },
-      buttonText: {
-        fontSize: 18,
-        fontFamily: 'Gill Sans',
+    },
+    titleText: {
+        fontSize: 25,
         textAlign: 'center',
-        margin: 10,
+        marginVertical: 15,
         color: '#ffffff',
-        backgroundColor: 'transparent',
-      },
+    },
+    image: {
+        height: 100,
+        width: 100,
+    },
+    subTitleText: {
+        width: 250,
+        fontSize: 15,
+        textAlign: 'center',
+        marginVertical: 0,
+        color: '#ffffff',
+    }
 });
 
 export default SplashScreen;
