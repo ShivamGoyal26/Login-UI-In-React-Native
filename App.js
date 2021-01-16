bc5e02286add75dd54caffb931dffbc38d311582
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-16 12:17:21 +0530

    Login and signup UI

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import SplashScreen from './SplashScrren/SplashScreen';
+
+export default function APP() {
+  return (
+      <SplashScreen></SplashScreen>
+    
+  );
+}
